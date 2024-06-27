aed2eeb7c113605ab6e62850d5807062de4e1ac8
yungwknd <yungwkndllc@gmail.com>
2024-06-27 11:04:36 -0700

    fix logic
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- src/app/seamless/lend/route.tsx
+++ src/app/seamless/lend/route.tsx
@@ -4,23 +4,6 @@ import { frames } from "./lend";
 import { IMAGE_URL, VERCEL_URL } from "@/utils";
 
 const handleRequest = frames(async (ctx) => {
-  if (ctx.message?.transactionId) {
-    return {
-      image: IMAGE_URL,
-      imageOptions: {
-        aspectRatio: "1:1",
-      },
-      buttons: [
-        <Button
-          action="link"
-          target={`https://basescan.org/tx/${ctx.message.transactionId}`}
-        >
-          View on basescan
-        </Button>,
-      ],
-    };
-  }
-
   return {
     image: IMAGE_URL,
     imageOptions: {
@@ -31,7 +14,7 @@ const handleRequest = frames(async (ctx) => {
       <Button
         action="tx"
         target={`${VERCEL_URL}/seamless/lend/tx`}
-        post_url="/"
+        post_url="/tx"
       >
         Lend
       </Button>,
--- src/app/seamless/route.tsx
+++ src/app/seamless/route.tsx
@@ -4,23 +4,6 @@ import { frames } from "./seamless";
 import { IMAGE_URL, VERCEL_URL } from "@/utils";
 
 const handleRequest = frames(async (ctx) => {
-  if (ctx.message?.transactionId) {
-    return {
-      image: IMAGE_URL,
-      imageOptions: {
-        aspectRatio: "1:1",
-      },
-      buttons: [
-        <Button
-          action="link"
-          target={`https://basescan.org/tx/${ctx.message.transactionId}`}
-        >
-          View on basescan
-        </Button>,
-      ],
-    };
-  }
-
   return {
     image: IMAGE_URL,
     imageOptions: {
