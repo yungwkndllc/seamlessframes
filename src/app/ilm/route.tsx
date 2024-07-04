f718cd40b4214849d38000f55666e21da41852fd
yungwknd <yungwkndllc@gmail.com>
2024-07-03 23:23:35 -0700

    bigger
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- src/app/ilm/route.tsx
+++ src/app/ilm/route.tsx
@@ -8,68 +8,6 @@ import {
   VERCEL_URL,
 } from "@/utils";
 
-function convertTextToPath(textElementString: string): string {
-  // Regular expression to extract attributes and text content
-  const regex = /<text\s+([^>]*)>([\s\S]*?)<\/text>/;
-  const match = textElementString.match(regex);
-
-  if (!match) {
-    throw new Error("Invalid <text> element string.");
-  }
-
-  // Extract attributes and text content
-  const attributesString = match[1].trim();
-  const textContent = match[2].trim();
-
-  // Parse attributes
-  const attrsRegex = /(\w+)=["']([^"']+)["']/g;
-  let attrMatch;
-  const parsedAttributes: Record<string, string> = {};
-
-  while ((attrMatch = attrsRegex.exec(attributesString)) !== null) {
-    const [, name, value] = attrMatch;
-    parsedAttributes[name] = value;
-  }
-
-  // Construct path element string
-  const pathElementString = `<path d="M0,0" fill="${
-    parsedAttributes.fill || "black"
-  }">${textContent}</path>`;
-
-  return pathElementString;
-}
-
-// Example usage:
-const textElementString = `
-  <text
-    x="50%"
-    y="85%"
-    font-family="Arial, sans-serif"
-    font-size="14"
-    fill="#6d9b30"
-    text-anchor="middle"
-    alignment-baseline="middle"
-  >
-    Boost wstETH
-  </text>
-`;
-
-const apyString = (strategyAPY: number) => {
-  return `
-  <text
-    x="50%"
-    y="20%"
-    font-family="Arial, sans-serif"
-    font-size="24"
-    fill="#6d9b30"
-    text-anchor="middle"
-    alignment-baseline="middle"
-    font-weight="bold"
-  >
-    ${strategyAPY}% APY
-  </text>`;
-};
-
 const handleRequest = frames(async (ctx) => {
   let strategyAPY = 0;
   try {
@@ -113,7 +51,7 @@ const handleRequest = frames(async (ctx) => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <div tw="absolute bottom-4 text-xl flex p-4 rounded-lg shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl">
+        <div tw="absolute bottom-4 text-6xl flex p-4 rounded-lg shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl">
           {strategyAPY}% APY with wstETH Boost
         </div>
       </div>
